879d28d910e9b5fcd509011f68f61a35358b7862
TheoNorris <theonorris@live.com>
2021-05-15 11:44:29 +0000

    Connected home and about page. Added basic html formats

--- home/views.py
+++ home/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>Home</h1>')
+    return render(request, 'home/home.html')
 
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request, 'home/about.html')
